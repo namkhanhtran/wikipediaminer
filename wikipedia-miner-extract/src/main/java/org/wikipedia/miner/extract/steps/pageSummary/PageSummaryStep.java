6a7cabd4dafec8de7344d01d908661168831d419
antoine-tran <tranatuan24@gmail.com>
2014-10-14 01:24:06 +0200

    fix small changes in version

--- wikipedia-miner-extract/src/main/java/org/wikipedia/miner/extract/steps/pageSummary/PageSummaryStep.java
+++ wikipedia-miner-extract/src/main/java/org/wikipedia/miner/extract/steps/pageSummary/PageSummaryStep.java
@@ -6,8 +6,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.avro.mapreduce.AvroJob;
-import org.apache.avro.mapred.AvroKey;
-import org.apache.avro.mapred.AvroValue;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.filecache.DistributedCache;
 import org.apache.hadoop.fs.FSDataInputStream;
