55f80e2a760355a97bf2eb8dd426a3c46ee22610
antoine-tran <tranatuan24@gmail.com>
2014-10-15 19:29:04 +0200

    update run-jars

--- run-jars.sh
+++ run-jars.sh
@@ -11,4 +11,4 @@ CLPD=$CLPA:/home/tuan.tran/executable/wikipediaminer/wikipedia-miner-extract/tar
 HCLPDA=$HCLPA,/home/tuan.tran/executable/wikipediaminer/wikipedia-miner-extract/target/wikipedia-miner-extract-1.0-SNAPSHOT.jar
 export HADOOP_CLASSPATH="$CLPD:$HADOOP_CLASSPATH"
 export HADOOP_MAPRED_HOME="/opt/cloudera/parcels/CDH-5.1.0-1.cdh5.1.0.p0.53/lib/hadoop-0.20-mapreduce"
-hadoop jar /home/tuan.tran/executable/wikipediaminer/wikipedia-miner-extract/target/wikipedia-miner-extract-1.0-SNAPSHOT.jar -libjars ${HCLPDA} -D mapred.task.timeout=1200000 $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ${21}
+hadoop jar /home/tuan.tran/executable/wikipediaminer/wikipedia-miner-extract/target/wikipedia-miner-extract-1.0-SNAPSHOT-job.jar -libjars ${HCLPDA} -D mapred.task.timeout=1200000 $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ${21}
